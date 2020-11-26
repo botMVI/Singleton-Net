15dd80e941615fe1838a0325cca08407f5fd7763
Mike <ilmivy1024@gmail.com>
2020-11-26 17:28:51 +0600

    Initial Commit

---
+++ Singleton&Net/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Singleton&Net
+//
+//  Created by Michael Iliouchkin on 26.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
